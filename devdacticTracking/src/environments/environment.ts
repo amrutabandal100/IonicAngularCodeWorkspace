a94a19e64ac5179f884f04defbf575de1a48eff8
amrutabandal100 <amrutabandal100@gmail.com>
2020-04-02 13:03:27 +0530

    Location Tracker

---
+++ devdacticTracking/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCJygq6Ad2FxmUtxvC0juanr71wNZnRm38",
+    authDomain: "amruta-eacb7.firebaseapp.com",
+    databaseURL: "https://amruta-eacb7.firebaseio.com",
+    projectId: "amruta-eacb7",
+    storageBucket: "amruta-eacb7.appspot.com",
+    messagingSenderId: "919231031863",
+    appId: "1:919231031863:web:7e93904365342ef98c6db2",
+    measurementId: "G-1C281KRXNP"
+  }
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
